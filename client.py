ac7243d681306f63720c4fdf22467593d204e151
Eris <mariojetullaji@gmail.com>
2023-10-11 22:26:22 +0100

    idk what to put here

--- client.py
+++ client.py
@@ -1,2 +1,23 @@
 import discord
 from discord.ext import commands
+import config
+import os
+
+token = config.token
+intents = discord.Intents.all()
+client = commands.Bot(intents=intents, command_prefix="?", description="I just got so emo I fell apart.")
+
+async def loadCogs():
+    for fileName in os.listdir("./cogs"):
+        if fileName.endswith(".py"):
+            await client.load_extension(f'cogs.{fileName[:-3]}')
+            print(f"{fileName[:-3]} cog loaded!")
+
+
+@client.event
+async def on_ready():
+    print("Client is running!")
+    await loadCogs()
+
+client.run(token)
+
