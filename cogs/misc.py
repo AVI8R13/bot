c7a2721718900bcdebcc77809edd8d8ff0c2d864
Eris <78118362+erismustdie@users.noreply.github.com>
2023-10-13 15:10:15 +0100

    juts commit from school to test at home

--- cogs/misc.py
+++ cogs/misc.py
@@ -40,5 +40,26 @@ class Misc(commands.Cog):
         else:
             await ctx.send("Tails!")
 
+    @commands.command()
+    async def eightball(self, ctx, *question):
+        responses = ["It is certain", "Without a doubt", "You may rely on it", "Yes, definately", "It is decidedly so", "As I see it, yes",
+                     "Most Likely", "Yes", "Outlook good", "Signs point to yes", "Reply hazy try again", "Better not tell you now",
+                       "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and try again", "Don't count on it",
+                       "Outlook not good", "My sources say no", "Very doubtful", "My reply is no"]
+        response = random.randint(0, len(responses))
+        eigthballEmbed = discord.Embed(
+            title = f"Eight ball :eight_ball:"
+        )
+        eightballEmbed.add_field(name=question, value = response, inline = True)
+        await ctx.send(embed=eigthballEmbed)
+
+    
+
+
+
+        
+
+
+
 async def setup(client):
     await client.add_cog(Misc(client))
